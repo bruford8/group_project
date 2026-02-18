2e694720fcf3ab0e103a0646a59fb1e1f4f14c4d
bruford8 <ap2545773@gmail.com>
2026-02-18 11:01:16 +0900

    Add files via upload

---
+++ ФИНАЛЬНАЯ ВЕРСИЯ ИШАК/main.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt6.QtWidgets import QApplication
+from computers_window import Schedule
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    window = Schedule()
+    window.showMaximized()
+
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
